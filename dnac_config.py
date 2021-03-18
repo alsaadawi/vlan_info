7c52dbece9df4c06a901be3751450903b4f5b52a
Ali Alsaadawi <alsaadawi>
2021-03-18 09:06:03 -0700

    Initial commit

---
+++ dnac_config.py
@@ -0,0 +1,5 @@
+import os
+DNAC=os.environ.get('DNAC','americas-dna.honeywell.com')
+DNAC_PORT=os.environ.get('DNAC_PORT', 443)
+DNAC_USER=os.environ.get('DNAC_USER', 'h297951')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD', 'Dec212121!@')
